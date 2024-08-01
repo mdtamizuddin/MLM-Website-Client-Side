d05eb29884de610a4f2b3e28e8c392d4cea937c5
linear-digital <tamizuddin.tr@gmail.com>
2024-08-01 20:42:32 +0600

    update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const connectDB = require("./util/mongodb");
 const getSetting = require("./Routes/Settings/getSetting");
 const authChecker = require("./util/authChecker");
 const updateSetting = require("./Routes/Settings/updateSetting");
-const userService = require("./Routes/User/user.service");
+
 const User = require("./Routes/User/user.model");
 const Withdraw = require("./Routes/WithDraw/withdraw.model");
 require("dotenv").config();
